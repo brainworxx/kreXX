d1d943a8f7f661968af654b1805a1a4b3daada29
guelzow <tobias.guelzow@brainworxx.de>
2017-03-25 10:41:36 +0100

    Moved the process classes to a more apropiate namespace

---
+++ src/analyse/routing/process/ProcessBacktrace.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * kreXX: Krumo eXXtended
+ *
+ * kreXX is a debugging tool, which displays structured information
+ * about any PHP object. It is a nice replacement for print_r() or var_dump()
+ * which are used by a lot of PHP developers.
+ *
+ * kreXX is a fork of Krumo, which was originally written by:
+ * Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
+ *
+ * @author
+ *   brainworXX GmbH <info@brainworxx.de>
+ *
+ * @license
+ *   http://opensource.org/licenses/LGPL-2.1
+ *
+ *   GNU Lesser General Public License Version 2.1
+ *
+ *   kreXX Copyright (C) 2014-2017 Brainworxx GmbH
+ *
+ *   This library is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation; either version 2.1 of the License, or (at
+ *   your option) any later version.
+ *   This library is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ *   for more details.
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software Foundation,
+ *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+namespace Brainworxx\Krexx\Analyse\Routing\Process;
+
+use Brainworxx\Krexx\Service\Factory\Pool;
+
+/**
+ * Processing of backtraces. No abstract for you, because we are dealing with
+ * an array here.
+ *
+ * @package Brainworxx\Krexx\Analyse\Routing\Process
+ */
+class ProcessBacktrace
+{
+    /**
+     * Here we store all relevant data.
+     *
+     * @var Pool
+     */
+    protected $pool;
+
+    /**
+     * Injects the pool.
+     *
+     * @param Pool $pool
+     *   The pool, where we store the classes we need.
+     */
+    public function __construct(Pool $pool)
+    {
+         $this->pool = $pool;
+    }
+
+    /**
+     * Analysis a backtrace.
+     *
+     * We need to format this one a little bit different than a
+     * normal array.
+     *
+     * @param array $backtrace
+     *   The backtrace.
+     * @param int $offset
+     *   For some reason, we have an offset of -1 for fatal error backtrace
+     *   line number.
+     *
+     * @return string
+     *   The rendered backtrace.
+     */
+    public function process(array &$backtrace, $offset = 0)
+    {
+        $output = '';
+
+        foreach ($backtrace as $step => $stepData) {
+            $output .= $this->pool->render->renderExpandableChild(
+                $this->pool->createClass('Brainworxx\\Krexx\\Analyse\\Model')
+                    ->setName($step)
+                    ->setType('Stack Frame')
+                    ->addParameter('data', $stepData)
+                    ->addParameter('offset', $offset)
+                    ->injectCallback(
+                        $this->pool->createClass('Brainworxx\\Krexx\\Analyse\\Callback\\Analyse\\BacktraceStep')
+                    )
+            );
+        }
+
+        return $output;
+    }
+}
