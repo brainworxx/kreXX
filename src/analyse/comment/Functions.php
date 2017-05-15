25f0020a71524220ce9216931e576cf3dea4f36e
guelzow <tobias.guelzow@brainworxx.de>
2017-05-15 13:21:14 +0200

    Introduced a static cache for the comment analysis.

--- src/analyse/comment/Functions.php
+++ src/analyse/comment/Functions.php
@@ -49,6 +49,14 @@ class Functions extends AbstractComment
      */
     public function getComment($reflection, \ReflectionClass $reflectionClass = null)
     {
-        return $this->pool->encodeString($this->prettifyComment($reflection->getDocComment()));
+        // Do some static caching. The comment will not change during a run.
+        static $cache = array();
+        $cachingKey = $reflectionClass->getName();
+
+        if (!isset($cache[$cachingKey])) {
+            // Cache not found. We need to generate this one.
+            $cache[$cachingKey] = $this->pool->encodeString($this->prettifyComment($reflection->getDocComment()));
+        }
+        return $cache[$cachingKey];
     }
 }
