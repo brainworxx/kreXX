7237d63f978f2582981fed329c8f93de1e3caa52
guelzow <tobias.guelzow@brainworxx.de>
2018-07-07 09:58:57 +0200

    Moved the bootstrapping to it's own file.

--- Krexx.php
+++ Krexx.php
@@ -32,13 +32,12 @@
  *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+// Include some files and set some internal values.
+include_once 'bootstrap.php';
+
 use Brainworxx\Krexx\Service\Factory\Pool;
 use Brainworxx\Krexx\Controller\AbstractController;
 use Brainworxx\Krexx\Service\Config\Fallback;
-use Brainworxx\Krexx\Service\Config\Config;
-
-// Include some files and set some internal values.
-\Krexx::bootstrapKrexx();
 
 /**
  * Public functions, allowing access to the kreXX debug features.
@@ -63,152 +62,6 @@ class Krexx
      * @var bool
      */
     protected static $disabledByPhp = false;
-
-    /**
-     * Includes all needed files and sets some internal values.
-     *
-     * @internal
-     */
-    public static function bootstrapKrexx()
-    {
-        // There may or may not be an active autoloader, which may or may not
-        // be able to autolaod the krexx files. There amy or may not be an
-        // unwanted interaction with the rest of the system when registering
-        // another autoloader. This leaves us with loading every single file
-        // via include_once.
-        define('KREXX_DIR', __DIR__ . DIRECTORY_SEPARATOR);
-        include_once 'src/Analyse/Callback/AbstractCallback.php';
-
-        include_once 'src/Analyse/Callback/Analyse/Objects/AbstractObjectAnalysis.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/Constants.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/DebugMethods.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/Getter.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/Methods.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/PrivateProperties.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/ProtectedProperties.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/PublicProperties.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects/Traversable.php';
-
-        include_once 'src/Analyse/Callback/Analyse/BacktraceStep.php';
-        include_once 'src/Analyse/Callback/Analyse/ConfigSection.php';
-        include_once 'src/Analyse/Callback/Analyse/Debug.php';
-        include_once 'src/Analyse/Callback/Analyse/Objects.php';
-
-        include_once 'src/Analyse/Callback/Iterate/ThroughArray.php';
-        include_once 'src/Analyse/Callback/Iterate/ThroughConfig.php';
-        include_once 'src/Analyse/Callback/Iterate/ThroughConstants.php';
-        include_once 'src/Analyse/Callback/Iterate/ThroughGetter.php';
-        include_once 'src/Analyse/Callback/Iterate/ThroughLargeArray.php';
-        include_once 'src/Analyse/Callback/Iterate/ThroughMethodAnalysis.php';
-        include_once 'src/Analyse/Callback/Iterate/ThroughMethods.php';
-        include_once 'src/Analyse/Callback/Iterate/ThroughProperties.php';
-
-        include_once 'src/Analyse/Caller/AbstractCaller.php';
-        include_once 'src/Analyse/Caller/CallerFinder.php';
-
-        include_once 'src/Analyse/Code/Codegen.php';
-        include_once 'src/Analyse/Code/Connectors.php';
-        include_once 'src/Analyse/Code/Scope.php';
-
-        include_once 'src/Analyse/Comment/AbstractComment.php';
-        include_once 'src/Analyse/Comment/Functions.php';
-        include_once 'src/Analyse/Comment/Methods.php';
-        include_once 'src/Analyse/Comment/Properties.php';
-
-        include_once 'src/Analyse/Routing/AbstractRouting.php';
-        include_once 'src/Analyse/Routing/Routing.php';
-
-        include_once 'src/Analyse/Routing/Process/AbstractProcess.php';
-        include_once 'src/Analyse/Routing/Process/ProcessArray.php';
-        include_once 'src/Analyse/Routing/Process/ProcessBacktrace.php';
-        include_once 'src/Analyse/Routing/Process/ProcessBoolean.php';
-        include_once 'src/Analyse/Routing/Process/ProcessClosure.php';
-        include_once 'src/Analyse/Routing/Process/ProcessFloat.php';
-        include_once 'src/Analyse/Routing/Process/ProcessInteger.php';
-        include_once 'src/Analyse/Routing/Process/ProcessNull.php';
-        include_once 'src/Analyse/Routing/Process/ProcessObject.php';
-        include_once 'src/Analyse/Routing/Process/ProcessResource.php';
-        include_once 'src/Analyse/Routing/Process/ProcessString.php';
-        include_once 'src/Analyse/Routing/Process/ProcessOther.php';
-
-        include_once 'src/Analyse/AbstractModel.php';
-        include_once 'src/Analyse/Model.php';
-
-        include_once 'src/Controller/AbstractController.php';
-        include_once 'src/Controller/BacktraceController.php';
-        include_once 'src/Controller/DumpController.php';
-        include_once 'src/Controller/TimerController.php';
-        include_once 'src/Controller/EditSettingsController.php';
-        include_once 'src/Controller/ErrorController.php';
-
-        include_once 'src/Errorhandler/AbstractError.php';
-        include_once 'src/Errorhandler/Fatal.php';
-
-        include_once 'src/Service/Config/Fallback.php';
-        include_once 'src/Service/Config/Config.php';
-        include_once 'src/Service/Config/Model.php';
-        include_once 'src/Service/Config/Security.php';
-
-        include_once 'src/Service/Config/From/Cookie.php';
-        include_once 'src/Service/Config/From/Ini.php';
-
-        include_once 'src/Service/Factory/EventHandlerInterface.php';
-        include_once 'src/Service/Factory/Event.php';
-        include_once 'src/Service/Factory/Factory.php';
-        include_once 'src/Service/Factory/Pool.php';
-
-        include_once 'src/Service/Flow/Emergency.php';
-        include_once 'src/Service/Flow/Recursion.php';
-
-        include_once 'src/Service/Misc/Encoding.php';
-        include_once 'src/Service/Misc/File.php';
-        include_once 'src/Service/Misc/Registry.php';
-        include_once 'src/Service/Misc/ReflectionUndeclaredProperty.php';
-
-        include_once 'src/Service/Plugin/Registration.php';
-        include_once 'src/Service/Plugin/PluginConfigInterface.php';
-
-        include_once 'src/View/Output/AbstractOutput.php';
-        include_once 'src/View/Output/Chunks.php';
-        include_once 'src/View/Output/File.php';
-        include_once 'src/View/Output/Browser.php';
-
-        include_once 'src/View/RenderInterface.php';
-        include_once 'src/View/AbstractRender.php';
-        include_once 'src/View/Messages.php';
-        include_once 'src/View/Render.php';
-
-        // Point the configuration to the right directories
-        Config::$directories = array(
-            'chunks' => KREXX_DIR . 'chunks/',
-            'log' => KREXX_DIR . 'log/',
-            'config' => KREXX_DIR . 'config/Krexx.ini',
-        );
-
-        if (!function_exists('krexx')) {
-            /**
-             * Alias function for object analysis.
-             *
-             * Register an alias function for object analysis,
-             * so you will not have to type \Krexx::open($data);
-             * all the time.
-             *
-             * @param mixed $data
-             *   The variable we want to analyse.
-             * @param string $handle
-             *   The developer handle.
-             */
-            function krexx($data = null, $handle = '')
-            {
-                if (empty($handle)) {
-                    \Krexx::open($data);
-                    return;
-                }
-
-                \Krexx::$handle($data);
-            }
-        }
-    }
 
     /**
      * Handles the developer handle.
