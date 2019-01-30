261a992e746deee875f475fb3a84a6504b7dfb5a
guelzow <tobias.guelzow@brainworxx.de>
2019-01-30 14:37:54 +0100

    Bugfix: Property analysis does not handle predefined classes correctly

--- src/Analyse/ConstInterface.php
+++ src/Analyse/ConstInterface.php
@@ -75,4 +75,5 @@ interface ConstInterface
     const PARAM_ADDITIONAL = 'additional';
 
     const UNKNOWN_VALUE = '. . .';
+    const UNKNOWN_DECLARATION = ":: unable to determine declaration :: \n\nMaybe this is a predeclared class?";
 }
