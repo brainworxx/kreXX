f995234c58d2c1e8b36da7a9414b1935b838fa9a
guelzow <tobias.guelzow@brainworxx.de>
2019-04-25 14:42:56 +0200

    Generalized the tests for the backtrace controller.

--- tests/Controller/BacktraceControllerTest.php
+++ tests/Controller/BacktraceControllerTest.php
@@ -36,45 +36,15 @@ namespace Brainworxx\Krexx\Tests\Controller;
 
 use Brainworxx\Krexx\Analyse\Callback\Iterate\ThroughConfig;
 use Brainworxx\Krexx\Analyse\Caller\CallerFinder;
-use Brainworxx\Krexx\Analyse\Code\Scope;
-use Brainworxx\Krexx\Analyse\ConstInterface;
 use Brainworxx\Krexx\Analyse\Model;
 use Brainworxx\Krexx\Analyse\Routing\Process\ProcessBacktrace;
 use Brainworxx\Krexx\Controller\BacktraceController;
 use Brainworxx\Krexx\Krexx;
-use Brainworxx\Krexx\Service\Config\Config;
-use Brainworxx\Krexx\Service\Config\Fallback;
-use Brainworxx\Krexx\Service\Factory\Pool;
 use Brainworxx\Krexx\Service\Flow\Emergency;
-use Brainworxx\Krexx\Service\Misc\File;
-use Brainworxx\Krexx\Tests\Helpers\AbstractTest;
 use Brainworxx\Krexx\Tests\Helpers\CallbackNothing;
-use Brainworxx\Krexx\Tests\Helpers\RenderNothing;
-use Brainworxx\Krexx\View\Messages;
-use Brainworxx\Krexx\View\Output\Browser;
-use Brainworxx\Krexx\View\Output\Chunks;
 
-class BacktraceControllerTest extends AbstractTest
+class BacktraceControllerTest extends AbstractController
 {
-    /**
-     * Result mock from the caller finder.
-     *
-     * @var array
-     */
-    protected $callerFinderResult;
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->callerFinderResult = [
-            ConstInterface::TRACE_FILE => 'just another path',
-            ConstInterface::TRACE_LINE => 41,
-            ConstInterface::TRACE_VARNAME => '$varWithAName',
-            ConstInterface::TRACE_TYPE => 'Backtrace',
-        ];
-    }
-
     /**
      * Testing of the backtrace action, with too many calls before.
      *
@@ -99,7 +69,7 @@ class BacktraceControllerTest extends AbstractTest
     }
 
     /**
-     * Testing a simple backtrace, no special stuff.
+     * Testing a simple backtrace.
      *
      * @covers \Brainworxx\Krexx\Controller\BacktraceController::backtraceAction
      * @covers \Brainworxx\Krexx\Controller\BacktraceController::outputFooter
@@ -109,10 +79,8 @@ class BacktraceControllerTest extends AbstractTest
     public function testBacktraceAction()
     {
         $backtraceController = new BacktraceController(Krexx::$pool);
+        $poolMock = $this->mockMainOutput($backtraceController);
 
-        $poolMock = $this->createMock(Pool::class);
-        $poolMock->expects($this->once())
-            ->method('reset');
         $proccessMock = $this->createMock(ProcessBacktrace::class);
         $proccessMock->expects($this->once())
             ->method('process')
@@ -132,116 +100,8 @@ class BacktraceControllerTest extends AbstractTest
                     [ThroughConfig::class, new CallbackNothing(Krexx::$pool)]
                 ]
             ));
-        $this->setValueByReflection('pool', $poolMock, $backtraceController);
-
-
-        // And now mock the living hell outa this little bugger.
-        $callerFinderMock = $this->createMock(CallerFinder::class);
-        $callerFinderMock->expects($this->once())
-            ->method('findCaller')
-            ->will($this->returnValue($this->callerFinderResult));
-        $this->setValueByReflection('callerFinder', $callerFinderMock, $backtraceController);
-
-        $scopeMock = $this->createMock(Scope::class);
-        $scopeMock->expects($this->once())
-            ->method('setScope')
-            ->with($this->callerFinderResult[ConstInterface::TRACE_VARNAME]);
-        $poolMock->scope = $scopeMock;
-
-        $chunksMock = $this->createMock(Chunks::class);
-        $chunksMock->expects($this->once())
-            ->method('detectEncoding')
-            ->with('generated HTML code');
-        $chunksMock->expects($this->once())
-            ->method('addMetadata')
-            ->with($this->callerFinderResult);
-        $poolMock->chunks = $chunksMock;
-
-        $emergencyMock = $this->createMock(Emergency::class);
-        $emergencyMock->expects($this->once())
-            ->method('checkMaxCall')
-            ->will($this->returnValue(false));
-        $emergencyMock->expects($this->once())
-            ->method('checkEmergencyBreak')
-            ->will($this->returnValue(false));
-        $poolMock->emergencyHandler = $emergencyMock;
-
-        $renderNothing = new RenderNothing(Krexx::$pool);
-        $poolMock->render = $renderNothing;
-
-        $outputServiceMock = $this->createMock(Browser::class);
-        $outputServiceMock->expects($this->exactly(3))
-            ->method('addChunkString')
-            ->withAnyParameters()
-            ->willReturnSelf();
-        $this->setValueByReflection('outputService', $outputServiceMock, $backtraceController);
 
         $this->mockFooterHeaderOutput($poolMock);
         $backtraceController->backtraceAction();
-
-        // Not really any assertions here, most of the stuff is already in the
-        // mocks.
-    }
-
-    /**
-     * Creating all the mocks for the footer output.
-     */
-    protected function mockFooterHeaderOutput($poolMock)
-    {
-        $pathToIni = 'some path';
-        $pathToSkin = 'skin directory';
-
-        $configMock = $this->createMock(Config::class);
-        $configMock->expects($this->once())
-            ->method('getPathToIniFile')
-            ->will($this->returnValue($pathToIni));
-        $configMock->expects($this->once())
-            ->method('getSetting')
-            ->with(Fallback::SETTING_DESTINATION)
-            ->will($this->returnValue('some destination'));
-        $configMock->expects($this->once())
-            ->method('getSkinDirectory')
-            ->will($this->returnValue($pathToSkin));
-        $poolMock->config = $configMock;
-
-        $fileServiceMock = $this->createMock(File::class);
-        $fileServiceMock->expects($this->exactly(3))
-            ->method('fileIsReadable')
-            ->withConsecutive(
-                [$pathToIni],
-                [KREXX_DIR . 'resources/jsLibs/kdt.min.js'],
-                [$pathToSkin . 'krexx.min.js']
-            )->will($this->returnValueMap(
-                [
-                    [$pathToIni, true],
-                    [KREXX_DIR . 'resources/jsLibs/kdt.min.js', true],
-                    [$pathToSkin . 'krexx.min.js', true]
-                ]
-            ));
-        $fileServiceMock->expects($this->once())
-            ->method('filterFilePath')
-            ->with($pathToIni)
-            ->will($this->returnValue('filtered path'));
-        $fileServiceMock->expects($this->exactly(3))
-            ->method('getFileContents')
-            ->withConsecutive(
-                [KREXX_DIR . 'resources/jsLibs/kdt.min.js'],
-                [$pathToSkin . 'skin.css'],
-                [$pathToSkin . 'krexx.min.js']
-            )->will($this->returnValueMap(
-                [
-                    [KREXX_DIR . 'resources/jsLibs/kdt.min.js', true, 'some js'],
-                    [$pathToSkin . 'skin.css', true, 'some styles'],
-                    [$pathToSkin . 'krexx.min.js', true, 'more js']
-                ]
-            ));
-        $poolMock->fileService = $fileServiceMock;
-
-        $messageMock = $this->createMock(Messages::class);
-        $messageMock->expects($this->once())
-            ->method('getHelp')
-            ->with('currentConfig')
-            ->will($this->returnValue('some helpful description'));
-        $poolMock->messages = $messageMock;
     }
 }
