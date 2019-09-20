1614b98335a211dd52712ef156fa1cdba3951cdd
guelzow <tobias.guelzow@brainworxx.de>
2019-09-20 14:09:44 +0200

    Updated the unit tests, so they work from another root directory.

--- tests/Analyse/Caller/CallerFinderTest.php
+++ tests/Analyse/Caller/CallerFinderTest.php
@@ -70,7 +70,7 @@ class CallerFinderTest extends AbstractTest
 
         // Create our test subject.
         $this->callerFinder = new CallerFinder(Krexx::$pool);
-        $this->pathToFixture = '...' . DIRECTORY_SEPARATOR . 'tests' .
+        $this->pathToFixture = DIRECTORY_SEPARATOR . 'tests' .
             DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . 'ComplexMethodFixture.php';
     }
 
@@ -140,7 +140,7 @@ class CallerFinderTest extends AbstractTest
         $result = $this->callerFinder->findCaller('', $this->subjectVar);
 
         // Check the result
-        $this->assertEquals($this->pathToFixture, $result[ConstInterface::TRACE_FILE]);
+        $this->assertStringEndsWith($this->pathToFixture, $result[ConstInterface::TRACE_FILE]);
         $this->assertEquals(74, $result[ConstInterface::TRACE_LINE]);
         $this->assertEquals('$parameter', $result[ConstInterface::TRACE_VARNAME]);
         $this->assertEquals('Analysis of $parameter, string', $result[ConstInterface::TRACE_TYPE]);
@@ -167,7 +167,7 @@ class CallerFinderTest extends AbstractTest
         $result = $this->callerFinder->findCaller(static::HEADLINE_STRING, $this->subjectVar);
 
         // Check the result
-        $this->assertEquals($this->pathToFixture, $result[ConstInterface::TRACE_FILE]);
+        $this->assertStringEndsWith($this->pathToFixture, $result[ConstInterface::TRACE_FILE]);
         $this->assertEquals(74, $result[ConstInterface::TRACE_LINE]);
         $this->assertEquals('$parameter', $result[ConstInterface::TRACE_VARNAME]);
         $this->assertEquals(static::HEADLINE_STRING, $result[ConstInterface::TRACE_TYPE]);
@@ -197,7 +197,7 @@ class CallerFinderTest extends AbstractTest
         $result = $this->callerFinder->findCaller(static::HEADLINE_STRING, $this->subjectVar);
 
         // Check the result
-        $this->assertEquals($this->pathToFixture . ' file not there', $result[ConstInterface::TRACE_FILE]);
+        $this->assertStringEndsWith($this->pathToFixture . ' file not there', $result[ConstInterface::TRACE_FILE]);
         $this->assertEquals(74, $result[ConstInterface::TRACE_LINE]);
         $this->assertEquals('. . .', $result[ConstInterface::TRACE_VARNAME]);
         $this->assertEquals(static::HEADLINE_STRING, $result[ConstInterface::TRACE_TYPE]);
