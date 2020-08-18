5f1193bf91de693e06e1d3173745d6378dbf4117
guelzow <tobias.guelzow@brainworxx.de>
2020-08-18 17:19:25 +0200

    Simplified the output of the filepath analysis.

--- src/Analyse/Callback/Analyse/Scalar/FilePath.php
+++ src/Analyse/Callback/Analyse/Scalar/FilePath.php
@@ -58,13 +58,6 @@ class FilePath extends AbstractScalarAnalysis implements ViewConstInterface
     protected $bufferInfo;
 
     /**
-     * The path we are analysing.
-     *
-     * @var string
-     */
-    protected $path = '';
-
-    /**
      * No file path analysis without the finfo class.
      *
      * @return bool
@@ -114,14 +107,22 @@ class FilePath extends AbstractScalarAnalysis implements ViewConstInterface
 
         try {
             $isFile = is_file($string);
+            $realPath = realpath($string);
         } catch (TypeError $exception) {
             $isFile = false;
         }
 
+        if ($isFile === true) {
+            if ($string !== $realPath) {
+                // We only add the realpath, if it differs from the string
+                $model->addToJson('Real path', is_string($realPath) === true ? $realPath : 'n/a');
+            }
+            $model->addToJson(static::META_MIME_TYPE, $this->bufferInfo->file($string));
+        }
+
         restore_error_handler();
 
-        $this->path = $string;
-        return $isFile;
+        return false;
     }
 
     /**
@@ -129,12 +130,6 @@ class FilePath extends AbstractScalarAnalysis implements ViewConstInterface
      */
     protected function handle(): array
     {
-        $realPath = realpath($this->path);
-
-        $meta = [];
-        $meta['Real path'] = is_string($realPath) === true ? $realPath : 'n/a';
-        $meta[static::META_MIME_TYPE] = $this->bufferInfo->file($this->path);
-
-        return $meta;
+        return [];
     }
 }
