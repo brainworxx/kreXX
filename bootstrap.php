82af55c3ef554f9732736622b2d45d1d6bc15f03
guelzow <tobias.guelzow@brainworxx.de>
2022-01-13 13:46:01 +0100

    Deprecated the ControllerConstInterface.

--- bootstrap.php
+++ bootstrap.php
@@ -59,7 +59,9 @@ call_user_func(function () {
         include_once KREXX_DIR . 'src/Analyse/Callback/CallbackConstInterface.php';
         include_once KREXX_DIR . 'src/Analyse/Routing/Process/ProcessConstInterface.php';
         include_once KREXX_DIR . 'src/Analyse/Caller/BacktraceConstInterface.php';
+        // deprecated
         include_once KREXX_DIR . 'src/Controller/ControllerConstInterface.php';
+        // deprecated
         include_once KREXX_DIR . 'src/Service/Config/ConfigConstInterface.php';
         include_once KREXX_DIR . 'src/Analyse/Code/CodegenConstInterface.php';
         include_once KREXX_DIR . 'src/Analyse/Code/ConnectorsConstInterface.php';
