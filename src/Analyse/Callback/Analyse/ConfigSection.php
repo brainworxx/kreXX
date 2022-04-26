8c6f8545ca3d8b1b1c034210d82dba49fb7aa769
guelzow <tobias.guelzow@brainworxx.de>
2022-04-26 09:04:24 +0200

    Deprecated getEditable(), setAllowCodeGen(), getAllowCodegen(), setChunksAreAllowed(), getChunksAreAllowed(), getLoggingIsAllowed(), setLoggingIsAllowed()

--- src/Analyse/Callback/Analyse/ConfigSection.php
+++ src/Analyse/Callback/Analyse/ConfigSection.php
@@ -93,7 +93,7 @@ class ConfigSection extends AbstractCallback implements CallbackConstInterface, 
         $model = $this->pool->createClass(Model::class)->setHelpid($id . 'Help');
         $name = $this->pool->messages->getHelp($id . 'Readable');
         $value = $this->prepareValue($setting);
-        if ($setting->getEditable()) {
+        if ($setting->isEditable()) {
             return $this->pool->render->renderSingleEditableChild(
                 $model->setData($name)
                     ->setName($value)
