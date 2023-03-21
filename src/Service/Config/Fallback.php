e63710241b4dc0c7eaa58bfe44e3e23797e6f957
guelzowbap\guelzow <tobias.guelzow@brainworxx.de>
2023-03-21 08:28:54 +0100

    Change: Set the nesting level to 10.

--- src/Service/Config/Fallback.php
+++ src/Service/Config/Fallback.php
@@ -328,7 +328,7 @@ abstract class Fallback implements ConfigConstInterface
             static::SETTING_ANALYSE_SCALAR => $this->returnBoolSelectTrue(static::SECTION_PROPERTIES),
             static::SETTING_ANALYSE_TRAVERSABLE => $this->returnBoolSelectTrue(static::SECTION_PROPERTIES),
             static::SETTING_DEBUG_METHODS => $this->returnDebugMethods(),
-            static::SETTING_NESTING_LEVEL => $this->returnInput(static::SECTION_PRUNE, 5),
+            static::SETTING_NESTING_LEVEL => $this->returnInput(static::SECTION_PRUNE, 10),
             static::SETTING_MAX_CALL => $this->returnInput(static::SECTION_EMERGENCY, 10),
             static::SETTING_DISABLED => $this->returnBoolSelectFalse(static::SECTION_OUTPUT),
             static::SETTING_DESTINATION => $this->returnDestination(),
