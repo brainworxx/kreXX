d1d30b3306bdb8a23c49541a4fbe59b69ac3638b
guelzowbap\guelzow <tobias.guelzow@brainworxx.de>
2024-09-06 09:14:07 +0200

    - Clenaup the Classname analysis class - Added the Unit Test for it.

--- src/Analyse/Scalar/String/ClassName.php
+++ src/Analyse/Scalar/String/ClassName.php
@@ -1,12 +1,46 @@
 <?php
 
+/**
+ * kreXX: Krumo eXXtended
+ *
+ * kreXX is a debugging tool, which displays structured information
+ * about any PHP object. It is a nice replacement for print_r() or var_dump()
+ * which are used by a lot of PHP developers.
+ *
+ * kreXX is a fork of Krumo, which was originally written by:
+ * Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
+ *
+ * @author
+ *   brainworXX GmbH <info@brainworxx.de>
+ *
+ * @license
+ *   http://opensource.org/licenses/LGPL-2.1
+ *
+ *   GNU Lesser General Public License Version 2.1
+ *
+ *   kreXX Copyright (C) 2014-2024 Brainworxx GmbH
+ *
+ *   This library is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation; either version 2.1 of the License, or (at
+ *   your option) any later version.
+ *   This library is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ *   for more details.
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software Foundation,
+ *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+declare(strict_types=1);
+
 namespace Brainworxx\Krexx\Analyse\Scalar\String;
 
-use Brainworxx\Krexx\Analyse\Callback\Analyse\Objects\Meta;
-use Brainworxx\Krexx\Analyse\Callback\Iterate\ThroughMeta;
 use Brainworxx\Krexx\Analyse\Model;
 use Brainworxx\Krexx\Service\Reflection\ReflectionClass;
 use Throwable;
+use ReflectionException;
 
 class ClassName extends AbstractScalarAnalysis
 {
@@ -62,9 +96,10 @@ class ClassName extends AbstractScalarAnalysis
     protected function handle(): array
     {
         $messages = $this->pool->messages;
-        $meta = [
-            $messages->getHelp('metaReflection') => new ReflectionClass($this->handledValue)
-        ];
+        try {
+            $meta = [$messages->getHelp('metaReflection') => new ReflectionClass($this->handledValue)];
+        } catch (ReflectionException $e) {
+        }
 
         // Move the extra part into a nest, for better readability.
         if ($this->model->hasExtra()) {
